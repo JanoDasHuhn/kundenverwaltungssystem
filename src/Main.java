3a54353b10eb7e2a8c1e53200abf23702be3b4b9
JanoDasHuhn <janodashuhn@users.noreply.github.com>
2024-11-26 09:25:30 +0100

    - adresse geändert

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import kunden.JuristischePerson;
 
 public class Main {
     public static void main(String[] args) {
-        JuristischePerson siemens = new JuristischePerson("siemens","Wolrad Kreusler Straße 6",2,2,1,true,"Hauptstraße 8");
+        JuristischePerson siemens = new JuristischePerson("siemens","Altschauerberg 8",2,2,1,true,"Hauptstraße 8");
 
     }
 }
